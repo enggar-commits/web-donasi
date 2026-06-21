03b408f27fa54ebc7fe0cbf27e3ee484b43dba3d
enggar-commits <enggarrahmad513@gmail.com>
2026-06-21 23:05:24 +0700

    Hapus database.php dari gitignore dan upload ke github

---
+++ config/database.php
@@ -0,0 +1,14 @@
+<?php
+$host = getenv('MYSQLHOST') ?: 'localhost';
+$user = getenv('MYSQLUSER') ?: 'root';
+$pass = getenv('MYSQLPASSWORD') ?: '';
+$db   = getenv('MYSQLDATABASE') ?: 'db_donasi';
+$port = getenv('MYSQLPORT') ?: '3306';
+
+try {
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Koneksi Database Gagal: " . $e->getMessage());
+}
+?>
